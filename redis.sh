a20dff4bf5274630c30dcd8d1fe22286cf9fd53d
caicm <caicm98@163.com>
2020-10-09 14:34:16 +0800

    feat:redis

---
+++ redis.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+docker rm -f redis-for-log
+
+DIR=/data/redis-for-log
+mkdir -p $DIR
+chown -R 1001:1001 $DIR
+
+docker run -itd \
+    --name redis-for-log \
+    --restart always \
+    --sysctl net.core.somaxconn=100000 \
+    -m 1024m \
+    -v $DIR:/bitnami/redis/data \
+    -e "REDIS_PASSWORD=pAssw0rd" \
+    -p 6379:6379 \
+    redis:5.0.8
